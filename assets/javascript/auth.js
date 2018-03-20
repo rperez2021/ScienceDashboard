b11dca8ae995c6adcda5a864f897395a3060f75b
rperez2021 <rob.perez.flores@gmail.com>
2018-03-19 22:03:04 -0700

    Figuring out authentication changes

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -151,3 +151,28 @@ function toggleSignIn() {
   window.onload = function() {
     initApp();
   };
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      console.log(displayName)
+      var email = user.email;
+      console.log(email)
+      var emailVerified = user.emailVerified;
+      console.log(emailVerified)
+      var photoURL = user.photoURL;
+      console.log(photoURL)
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      console.log(uid)
+      var providerData = user.providerData;
+      console.log(providerData)
+document.getElementById('userinfo').textContent = displayName + email + emailVerified + "<img src="+photoURL+">"
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+  });
+  
