ddb66e77797f05e5295b708c5e358ec31c4d9836
rperez2021 <rob.perez.flores@gmail.com>
2018-03-21 23:13:17 -0700

    Changes Toggles and Added User Functionality

--- assets/javascript/user.js
+++ assets/javascript/user.js
@@ -1,35 +1,39 @@
-window.onload = function() {
-    userdata();
-    
-  };
-  var database = firebase.database();
+window.onload = function () {
+  userdata();
 
-  //This is not final probably jacked up
-  var database = firebase.database();
-  var userRef = database.ref("/users/");
-  var user;
-  // database.ref("/users/").on("value", function(snapshot) {
-  //   if (snapshot.child("user").exists()) {
-  //     console.log("user exists");
-  //     user = snapshot.val().player1;
-  //   };
-  // });
+};
+var database = firebase.database();
+
+//This is not final probably jacked up
+var database = firebase.database();
+var userRef = database.ref("/users/");
+var user;
+// database.ref("/users/").on("value", function(snapshot) {
+//   if (snapshot.child("user").exists()) {
+//     console.log("user exists");
+//     user = snapshot.val().player1;
+//   };
+// });
 // This is part of the bad stuff
 
-  function userdata(){
-  firebase.auth().onAuthStateChanged(function(user) {
+function userdata() {
+  firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       // User is signed in.
       var displayName = user.displayName;
-      //This is jacked up
-      database.ref().child("/users/user"+user.uid).set({
-        user: user.displayName, 
-        email: user.email, 
-        photo: user.photoURL, 
-        space: false, 
-        earthquake: false, 
-        airpollution: false, 
-        potd: false });
+
+      database.ref().child("/users/user" + user.uid).set({
+        user: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+        display:
+          {
+            space: false,
+            earthquake: false,
+            airpollution: false,
+            potd: false
+          }
+      });
       console.log(displayName)
       var email = user.email;
       console.log(email)
@@ -43,21 +47,21 @@ window.onload = function() {
       var providerData = user.providerData;
       console.log(providerData)
 
-      $("#username").text("Welcome! "+ displayName);
-      $("#userphoto").html("<img src='"+photoURL+"' class='rounded-circle' width='40' height='40'>");
+      $("#username").text("Welcome! " + displayName);
+      $("#userphoto").html("<img src='" + photoURL + "' class='rounded-circle' width='40' height='40'>");
       console.log("User " + displayName + " Is Signed In")
     } else {
       $("#username").text("User is not logged in");
-    console.log("User is Signed Out")
+      console.log("User is Signed Out")
     }
   });
 }
 
 $('#signout').on("click", function (event) {
   event.preventDefault()
-  firebase.auth().signOut().then(function() {
+  firebase.auth().signOut().then(function () {
     console.log('Signed Out');
-  }, function(error) {
+  }, function (error) {
     console.error('Sign Out Error', error);
   });
 });
